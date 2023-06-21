c055d99a76a6a7bbb29580c2e968396be2cd5692
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-06-21 19:59:49 -0400

    Start on landing page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="home">
     <navbar></navbar>
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <div id="home-container">
+    <div id="home-left">
+    <h1>Welcome, planties!</h1>
+    <p>Paroxysm of global death explorations quasar kindling the energy hidden in matter colonies of brilliant syntheses. Stirred by starlight concept of the number one stirred by starlight another world extraordinary claims require extraordinary evidence courage of our questions? Are creatures of the cosmos stirred by starlight two ghostly white figures in coveralls and helmets are softly dancing realm of the galaxies from which we spring made in the interiors of collapsing stars and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
+  </div>
+  <div id="home-right">
+    <img src="https://i.pinimg.com/736x/89/e9/71/89e971acf50a9f04a8f4525583e345aa.jpg" alt="Cute Robot Holding a Plant"/>      
+  </div>
+  </div>
   </div>
 </template>
 
@@ -16,13 +23,7 @@ export default {
 
 <style>
 
-/* grid-container {
-  display: grid;
-} */
-body {
-  /* https://www.eggradients.com/gradient/fresh */
-/* background-color: #378b29;
-background-image: linear-gradient(10deg, #378b29 0%, #ffffff 70%);
-background-size: stretch; */
+#home-container {
+  display: flex;
 }
 </style>
